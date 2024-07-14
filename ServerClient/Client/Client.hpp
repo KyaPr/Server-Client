5a5904688ec6d9412b88bd4c13e4182c0dfb3049
KyaPr <149680811+KyaPr@users.noreply.github.com>
2024-07-14 16:58:41 +0300

    Delete ServerClient directory

--- ServerClient/Client/Client.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <chrono>
-#include <cstdio>
-#include <thread>
-#include "httplib.h"
-using namespace httplib;
--- ServerClient/Server/Server.hpp
+++
@@ -1,8 +0,0 @@
-#define SERVER_CERT_FILE "./cert.pem"
-#define SERVER_PRIVATE_KEY_FILE "./key.pem"
-#include <iostream>
-#include <chrono>
-#include <cstdio>
-#include "database.h"
-#include "httplib.h"
-using namespace httplib;
